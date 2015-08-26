efb9dfce7cdca8bb1927d87759b17786671233f4
harish <harishdevara1992@gmail.com>
2015-08-26 13:13:20 -0400

    second commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,11 @@ class CategoriesController < ApplicationController
   # GET /categories.json
   def index
     @categories = Category.all
-
+     if params[:search]
+      @categories = Category.search(params [:search]).order("created_at DESC")
+    else
+    @categories = Category.all.order('created_at DESC')
+    end
     
   end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ActiveRecord::Base
 	has_many :products
 	validates :name, presence: true ,uniqueness: true
+
+end
+def self.search(search)
+  where("name LIKE ?", "%#{search}%") 
+  where("content LIKE ?", "%#{search}%")
 end
